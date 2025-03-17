63ac14e454827e8fcba618ac5d7ef0541bbdc698
Ranj <ranjdotdev@gmail.com>
2025-03-18 02:41:03 +0300

    Newlines

--- main.go
+++ main.go
@@ -5,19 +5,37 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
   data, err := os.Open("messages.txt")
   if err != nil { log.Fatal("error: ", err) }
   defer data.Close()
-
+  
   buffer := make([]byte, 8)
+  var currentLine string;
+  
   for {
+// 1st
     n, err := data.Read(buffer)
     if err == io.EOF {
       break
     }
-    fmt.Printf("read: %s\n", string(buffer[:n]))
+// 2nd
+parts := strings.Split(string(buffer[:n]), "\n")
+// 3rd
+for i, part := range parts {
+  if i != len(parts)-1 {
+    currentLine += part
+    fmt.Printf("read: %s\n", string(currentLine))
+    currentLine = ""
+    } else {
+      currentLine += part
+    }
+  }
+}
+  if currentLine != "" {
+    fmt.Printf("read: %s\n", string(currentLine))
   }
 }
