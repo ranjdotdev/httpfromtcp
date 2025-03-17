e20596e562e16f3cff664636d20f15ad958533a8
Ranj <ranjdotdev@gmail.com>
2025-03-18 01:19:37 +0300

    8 Bytes

--- main.go
+++ main.go
@@ -2,7 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
-func main(){
-  fmt.Println("I hope I get the job!")
+
+func main() {
+  data, err := os.Open("messages.txt")
+  if err != nil { log.Fatal("error: ", err) }
+  defer data.Close()
+
+  buffer := make([]byte, 8)
+  for {
+    n, err := data.Read(buffer)
+    if err == io.EOF {
+      break
+    }
+    fmt.Printf("read: %s\n", string(buffer[:n]))
+  }
 }
