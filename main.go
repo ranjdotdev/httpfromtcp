6e9e7e2c9b23fbfe2980beda12660d67b2727841
Ranj <ranjdotdev@gmail.com>
2025-03-18 17:28:09 +0300

    TCP

--- main.go
+++ main.go
@@ -4,18 +4,77 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 	"strings"
 )
 
-func main() {
-  data, err := os.Open("messages.txt")
-  if err != nil { log.Fatal("error: ", err) }
-  defer data.Close()
-  lines := getLinesChannel(data)
-  for line := range lines {
-    fmt.Printf("read: %s\n", line)
+type Message struct {
+  from    string
+  payload string
+}
+
+type Server struct{
+  listenAddr string
+  ln         net.Listener
+  quitch     chan struct{}
+  msgch      chan Message
+}
+
+func NewServer (listenAddr string) *Server {
+  return &Server{
+    listenAddr: listenAddr,
+    quitch: make(chan struct{}),
+    msgch:  make(chan Message, 8),
   }
+}
+
+func (s* Server) Start() error {
+  ln, err := net.Listen("tcp", s.listenAddr)
+  if err != nil {return err}
+  defer ln.Close();
+  s.ln = ln;
+
+  go s.acceptLoop()
+
+  <-s.quitch
+  close(s.msgch)
+
+  return nil
+}
+
+func (s *Server) acceptLoop () {
+  for {
+    conn, err := s.ln.Accept()
+    if err != nil {
+      fmt.Println("accept error: ", err)
+      continue;
+    }
+    fmt.Println("new connection: ", conn.RemoteAddr())
+    go s.readLoop(conn)
+  }
+}
+
+func (s *Server) readLoop(conn net.Conn) {
+  defer conn.Close();
+    lines := getLinesChannel(conn)
+    for line := range lines {
+        s.msgch <- Message{
+          from: conn.RemoteAddr().String(),
+          payload: line,
+        }
+    }
+  fmt.Println("connection closed: ", conn.RemoteAddr())
+}
+
+
+func main() {
+  server := NewServer(":42069")
+  go func(){
+    for msg := range server.msgch {
+      fmt.Printf("%s\n", msg.payload)
+    }
+    }()
+  log.Fatal(server.Start())
 }
 
 
@@ -24,15 +83,15 @@ func getLinesChannel(d io.ReadCloser) <-chan string {
   go func(){
     defer close(lines)
 
-    buffer := make([]byte, 8)
+    buf := make([]byte, 8)
     var currentLine string;
   
     for {
-      n, err := d.Read(buffer)
+      n, err := d.Read(buf)
       if err == io.EOF {
         break
       }
-      parts := strings.Split(string(buffer[:n]), "\n")
+      parts := strings.Split(string(buf[:n]), "\n")
       for i, part := range parts {
         if i != len(parts)-1 {
         currentLine += part
