790e4fbaa97fe592e98661dcbaa2c10ea478af2a
Ranj <ranjdotdev@gmail.com>
2025-03-18 03:42:43 +0300

    Channel Refactor

--- main.go
+++ main.go
@@ -12,30 +12,40 @@ func main() {
   data, err := os.Open("messages.txt")
   if err != nil { log.Fatal("error: ", err) }
   defer data.Close()
-  
-  buffer := make([]byte, 8)
-  var currentLine string;
-  
-  for {
-// 1st
-    n, err := data.Read(buffer)
-    if err == io.EOF {
-      break
-    }
-// 2nd
-parts := strings.Split(string(buffer[:n]), "\n")
-// 3rd
-for i, part := range parts {
-  if i != len(parts)-1 {
-    currentLine += part
-    fmt.Printf("read: %s\n", string(currentLine))
-    currentLine = ""
-    } else {
-      currentLine += part
-    }
+  lines := getLinesChannel(data)
+  for line := range lines {
+    fmt.Printf("read: %s\n", line)
   }
 }
-  if currentLine != "" {
-    fmt.Printf("read: %s\n", string(currentLine))
-  }
+
+
+func getLinesChannel(d io.ReadCloser) <-chan string {
+  lines := make(chan string)
+  go func(){
+    defer close(lines)
+
+    buffer := make([]byte, 8)
+    var currentLine string;
+  
+    for {
+      n, err := d.Read(buffer)
+      if err == io.EOF {
+        break
+      }
+      parts := strings.Split(string(buffer[:n]), "\n")
+      for i, part := range parts {
+        if i != len(parts)-1 {
+        currentLine += part
+        lines <- currentLine
+            currentLine = ""
+        } else {
+          currentLine += part
+        }
+      }
+    }
+    if currentLine != "" {
+      lines <- currentLine
+    }
+  }()
+return lines;
 }
